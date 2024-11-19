cd80eb71ed03282eb3c9645ebf007c6fe5f75b4a
Yan Liu <ly163com01@163.com>
2024-11-18 20:02:57 -0700

    Create SpaceInvader Project

---
+++ FinalProject/SpaceInvader_/SpaceInvader/EntryPoint.cpp
@@ -0,0 +1,19 @@
+/*
+	WinMain() is the entry point of a Windows program
+	(instead of the familiar main() in a console program)
+*/
+
+// Includes
+//=========
+
+#include "cSpaceInvader.h"
+
+// Entry Point
+//============
+
+int WINAPI WinMain( HINSTANCE i_thisInstanceOfTheApplication, HINSTANCE, char* i_commandLineArguments, int i_initialWindowDisplayState )
+{
+	const std::string vertexShaderPath = "data/Shaders/Vertex/standard.shader";
+	const std::string fragmentShaderPath = "data/Shaders/Fragment/animatedColor.shader";
+	return eae6320::Application::Run<eae6320::cSpaceInvader>( i_thisInstanceOfTheApplication, i_commandLineArguments, i_initialWindowDisplayState);
+}
